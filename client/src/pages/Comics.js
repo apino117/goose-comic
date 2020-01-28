eb0480f9d30e1567701a62e4e05ae259fe399787
Alex Pino <alex@Alexs-MacBook-Pro-3.local>
2020-01-28 15:54:35 -0500

    revamped comix page, made title "sheriff goose"

--- client/src/pages/Comics.js
+++ client/src/pages/Comics.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import Jumbotron from '../components/Jumbotron';
+import React, { Component } from "react";
+import Card from "../components/Card"
+import Jumbotron from "../components/Jumbotron";
 
+class Comics extends Component {
 
-const Comics = () => {
-    return (
-
-        <Jumbotron>
-            <h1 className="text-center">
-                <strong>Comic Archive</strong>
-            </h1>
-            <h2 className="text-center">All the comics from dead to zed.</h2>
-        </Jumbotron>
-
-    )
+    render() {
+        return (
+            <>
+                <Jumbotron>
+                    <h1 className="text-center">
+                        <strong>Comic Archive</strong>
+                    </h1>
+                    <h2 className="text-center">All comics from dead to zedd</h2>
+                </Jumbotron>
+                {/* <ComicBox></ComicBox> */}
+                <Card title="Title of Comic: Date">
+                    <div className="container" id="comic-box-container">
+                        <div className="row" id="comic-row">
+                            <div className="column" id="comic-column">
+                                <img className="Comic"
+                                    src={"https://static01.nyt.com/images/2008/06/02/business/02garfield.600.jpg?quality=75&auto=webp&disable=upscale"}
+                                    alt={"example"}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </>
+        );
+    }
 }
 
 export default Comics;
